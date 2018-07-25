269d6f0e5dad5cd9d5c3adf828ba958d9c13e8d8
Chely Ho <adept_inept@yahoo.com>
2018-07-25 21:02:09 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,6 @@
 class ProductSerializer < ActiveModel::Serializer
+  has_many :ordered_products
+  has_many :orders, :through => :ordered_products
+
   attributes :id, :name, :description, :inventory, :price
 end
